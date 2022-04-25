6836b86cbfde11896758dd37c32a3cb5b12bf2c4
Yzwcpm <1774570823@qq.com>
2022-04-25 17:26:03 +0800

    a

--- git_push.bat
+++ git_push.bat
@@ -1,9 +1,7 @@
 @echo off
 echo "========git add .=========="
 git add .
-echo "========修改了以下内容=========="
+echo "===========修改了以下内容=============="
 git status
-set /p input=输入后端本次提交的内容：
-git commit -m %input%
-git push git@github.com:Yzwcp/blog-koa-mysql.git master
+
 pause
